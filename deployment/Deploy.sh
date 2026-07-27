2607eb9f0578b5fe3be7fbe4cd80dda90754a465
Krishna Paudel <krishna444@gmail.com>
2026-07-27 12:43:00 +0200

    docker-compose cleanup and Deploy.sh script created

---
+++ deployment/Deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+FIRST_ARGUMENT="${1:---no-build}"
+if [[ "$FIRST_ARGUMENT" == "--help" ]]; then
+    echo "Deploy.sh [--build/--help/down]"
+elif [[ "$FIRST_ARGUMENT" == "--build" ]]; then
+    docker compose up -d --build
+elif [[ "$FIRST_ARGUMENT" == "--no-build" ]]; then
+    docker compose up -d 
+elif [[ "$FIRST_ARGUMENT" == "down" ]]; then
+    docker compose down
+fi
+#docker compose up -d --build
